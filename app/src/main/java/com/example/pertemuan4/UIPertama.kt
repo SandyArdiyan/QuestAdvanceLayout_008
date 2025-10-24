d1ae69e772cfc95c55520a3a2da16c2831c83e15
SandyArdiyan <sanardiyan881@gmail.com>
2025-10-24 22:21:09 +0700

    Mengganti Ukuran Fontsize di bagian univ

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -40,7 +40,7 @@ fun ActivitasPertama(modifier: Modifier){
 
         Text(
             text = stringResource(id = R.string.univ),
-            fontSize = 18.sp
+            fontSize = 20.sp
         )
 
         Spacer(modifier = Modifier.height(height = 25.dp))
@@ -64,7 +64,38 @@ fun ActivitasPertama(modifier: Modifier){
                     painter = gambar,
                     contentDescription = null,
                     modifier = Modifier
-                        .size(size =200.dp)
-                        .padding(all=6.dp)
+                        .size(size =100.dp)
+                        .padding(all=5.dp)
                 )
 
+                Spacer(modifier = Modifier.width(width = 30.dp))
+                Column(){
+                    Text(
+                        text=stringResource(id=R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top =15.dp)
+                    )
+                    Text(
+                        text = stringResource(id=R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ){
+                    Text(
+                        text =stringResource(id= R.string.copy),
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 50.dp)
+                    )
+                }
+            }
+        }
+    }
+}
