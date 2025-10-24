46aca8162fa0e59fbaf2679df132bb66b66fa53c
SandyArdiyan <sanardiyan881@gmail.com>
2025-10-24 22:16:32 +0700

    Mengganti color bagian nama

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -74,28 +74,6 @@ fun ActivitasPertama(modifier: Modifier){
                         text=stringResource(id=R.string.nama),
                         fontSize = 30.sp,
                         fontFamily = FontFamily.Cursive,
-                        color = Color.White,
+                        color = Color.Red,
                         modifier = Modifier.padding(top =15.dp)
                     )
-                    Text(
-                        text = stringResource(id=R.string.alamat),
-                        fontSize = 20.sp,
-                        color = Color.Yellow,
-                        modifier = Modifier.padding(top = 10.dp)
-                    )
-                }
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ){
-                    Text(
-                        text =stringResource(id= R.string.copy),,
-                        modifier = Modifier
-                            .align(Alignment.BottomCenter)
-                            .padding(bottom = 50.dp)
-                    )
-                }
-            }
-        }
-    }
-}
