a6d7cdf72a505e37672a6bb8e056d4e87c006003
SandyArdiyan <sanardiyan881@gmail.com>
2025-10-25 01:43:01 +0700

    Mengubah UIKedua Agar bisa di Run

--- app/src/main/java/com/example/pertemuan4/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan4/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pertemuan4.ui.theme.Pertemuan4Theme
+import com.example.pertemuan4.ui.theme.UIKedua
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    UIKedua(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pertemuan4Theme {
-        Greeting("Android")
     }
 }
